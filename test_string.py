13ce4a596763ae9448d31d6a4d34a31982da4ae4
Bhargav Mistry <bhargav.mistry@workday.com>
2020-08-19 15:07:06 -0700

    pushing string practice code to personal account (first push)
    Change-Id: Ibbed43e65d792aa9c8adbbc0525404d6cefa35a0

---
+++ test_string.py
@@ -0,0 +1,20 @@
+types_of_people = 10
+# Basic use of fstring
+x = f"There are {types_of_people} people in the room"
+print(x)
+
+#Function that will be called from fstring
+def to_lowercase(input):
+	return input.lower()
+
+name = "Bhargav Mistry"
+age = "32"
+length = 100
+# Calling function
+y=f"{to_lowercase(name)} is funny"
+print(y)
+
+# Calling method directly
+z=f"{name.upper()} is {age} years and his height is {length + 1} inches"
+print(z)
+
