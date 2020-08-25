2182232cb7e68cf993beafc3f2b8d6236f18f188
Bhargav Mistry <bhargav.mistry@workday.com>
2020-08-25 13:33:17 -0700

    Getting inputs from user
    Change-Id: I80bea4654099052c905f3cbe2f6bb33a27172f82

---
+++ getting_input.py
@@ -0,0 +1,15 @@
+print("What is your name?")
+name = input()
+# using end=' ' so that input doesn't go to the newline
+print("What is your age?", end=' ')
+age = input()
+
+location = input("Enter your location:")
+
+# typecasting user input to integer value, by default input() is string
+number1 = int(input("Enter number1:"))
+number2 = float(input("Enter number2:"))
+total = number1 + number2
+
+
+print(f"Hi {name}, you are {age} years old and live in {location}. Your total is: {total}")
