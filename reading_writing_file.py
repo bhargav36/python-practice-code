bfef234eb48ff227bb13be70d61532bb65b565cb
Bhargav Mistry <bhargav.mistry@workday.com>
2020-12-18 14:32:13 -0800

    Read and Write file with append flag
    Change-Id: I05a230252e3607b6a56103511aff821204bed07e

--- reading_writing_file.py
+++ reading_writing_file.py
@@ -10,7 +10,8 @@ print(f"We are going to truncate the {filename}, to proceed hit RETURN")
 input("?")
 
 print("Opening file....")
-target = open(filename,'w')
+### open the file in 'w' - write mode, 'a' - append mode
+target = open(filename,'a')
 
 print("Truncating file....")
 target.truncate()
@@ -23,11 +24,11 @@ line3 = input("3. ")
 
 print(f"Writing ^^ to file -> {filename}")
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write("%s \n%s \n%s \n" % (line1, line2, line3))
+#target.write("\n")
+#target.write(line2)
+#target.write("\n")
+#target.write(line3)
+#target.write("\n")
 
 target.close()
