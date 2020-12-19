16845fe1e52379e187fe11138b070a265b3664a8
Bhargav Mistry <bhargav.mistry@workday.com>
2020-12-18 16:13:18 -0800

    Copy content of one file to another
    Change-Id: I983d49c51d582ebcb2dcd9a71e0294391e5fbd81

---
+++ copy_file_content.py
@@ -0,0 +1,23 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+
+#static file name to read
+#from_file = "from_file.txt"
+print(f"Copying data from {from_file} to {to_file}")
+
+
+in_data = open(from_file).read()
+
+print(f"Does file exists? {exists(to_file)}")
+print(f"Hit Return to continue,")
+input()
+
+out_file = open(to_file, 'w')
+out_file.write(in_data)
+
+print(f"Done...")
+
+out_file.close()
